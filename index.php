009aad56e6a3627dd0a40c44641756b4f75d208e
LEKTATI Mahdi <lektati1@gmail.com>
2020-12-28 21:25:02 +0100

    heroku deploy

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<?php include("src/default.css") ?>
+<?php include("src/blue.css") ?>
+<?php include("src/green.css") ?>
+<?php include("src/purple.css") ?>
+<?php include("src/resume.pdf") ?>
+<?php include("src/script.js") ?>
+
 <?php include("src/index.html") ?>
